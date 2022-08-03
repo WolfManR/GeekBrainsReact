6cc4b4870fa8791b34a6cd9e53bfd4c7f0c299f2
Tarxos <ftarxos@gmail.com>
2022-08-03 18:30:55 +0300

    create message component and add to it styles

--- study/src/App.js
+++ study/src/App.js
@@ -1,5 +1,11 @@
+import Message from "./components/message/Message";
+
 function App() {
-  return <div> Hello React App</div>;
+  return (
+    <div>
+      <Message message={"Hello React App"} />
+    </div>
+  );
 }
 
 export default App;
