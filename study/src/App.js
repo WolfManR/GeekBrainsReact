79a7946fe2377d2627d75a2a2dc8e98165203485
Tarxos <ftarxos@gmail.com>
2022-08-06 13:39:55 +0300

    add message list and render it with map function

--- study/src/App.js
+++ study/src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Message from "./components/message/Message";
+import Tasks from "./components/tasks/Tasks";
 
 function App() {
   const displayMessage = "Hello React App";
-
+  const [messageList] = useState([
+    {
+      id: 1,
+      author: "John",
+      message: "Hello there",
+    },
+    {
+      id: 2,
+      author: "Lena",
+      message: "Hi",
+    },
+  ]);
   return (
     <div>
+      <Tasks />
       <Message message={displayMessage} />
+      {messageList.map((item) => (
+        <Message key={item.id} message={item.message} />
+      ))}
     </div>
   );
 }
