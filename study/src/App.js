05cd3bd8b1cabddbac6b0c7cbade3d1ff178c26e
Tarxos <ftarxos@gmail.com>
2022-08-06 19:01:54 +0300

    add behavior to send bot message after message send

--- study/src/App.js
+++ study/src/App.js
@@ -1,26 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import MessagesList from "./components/messagesList/MessagesList";
 import Tasks from "./components/tasks/Tasks";
 import "./App.css";
+
+const emptyMessage = { author: "", body: "" };
+const botMessage = { author: "Bot", body: "" };
 
 function App() {
   const [currentUser, setCurrentUser] = useState("John");
   const [messageList, setMessageList] = useState([]);
   const [idCounter, setIdCounter] = useState(0);
-  const emptyMessage = { author: "", body: "" };
-  const [formMessage, setMessage] = useState({ ...emptyMessage });
+  const [formMessage, setFormMessage] = useState({ ...emptyMessage });
 
-  const getNewId = () => {
+  const getNewId = useCallback(() => {
     const newId = idCounter + 1;
     setIdCounter(newId);
     return newId;
-  };
+  }, [idCounter]);
 
-  const addMessage = (e) => {
+  const addMessage = useCallback(
+    (newMessage) => {
+      setMessageList([...messageList, { ...newMessage, id: getNewId() }]);
+    },
+    [getNewId, messageList]
+  );
+
+  useEffect(() => {
+    if (messageList.length === 0) return;
+    const lastMessage = messageList.at(-1);
+    if (lastMessage.author === botMessage.author) return;
+    setTimeout(() => addMessage({ ...botMessage, body: `${lastMessage.author} write in chat` }), 1500);
+  }, [messageList, addMessage]);
+
+  const addMessageFromForm = (e) => {
     e.preventDefault();
-    const newMessage = { ...formMessage, id: getNewId() };
-    setMessageList([...messageList, newMessage]);
-    setMessage({ ...emptyMessage });
+    addMessage(formMessage);
+    setFormMessage({ ...emptyMessage });
   };
 
   return (
@@ -36,9 +51,9 @@ function App() {
         <MessagesList messages={messageList} currentUser={currentUser} />
 
         <form className="form-message">
-          <input type="text" placeholder="user" value={formMessage.author} onChange={(e) => setMessage({ ...formMessage, author: e.target.value })} />
-          <input type="text" placeholder="message" value={formMessage.body} onChange={(e) => setMessage({ ...formMessage, body: e.target.value })} />
-          <button onClick={addMessage}>send</button>
+          <input type="text" placeholder="user" value={formMessage.author} onChange={(e) => setFormMessage({ ...formMessage, author: e.target.value })} />
+          <input type="text" placeholder="message" value={formMessage.body} onChange={(e) => setFormMessage({ ...formMessage, body: e.target.value })} />
+          <button onClick={addMessageFromForm}>send</button>
         </form>
       </div>
     </div>
