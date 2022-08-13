0b4c0043eb970a00f81e4bf75a43115ef02aa7e2
Tarxos <ftarxos@gmail.com>
2022-08-14 01:13:52 +0300

    add not found page

--- study/src/App.js
+++ study/src/App.js
@@ -5,6 +5,7 @@ import SiteLayout from "./Layouts/SiteLayout";
 import Chat from "./pages/Chat";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
 const theme = createTheme({
   spacing: [0, 4, 8, 16],
@@ -39,6 +40,7 @@ function App() {
           <Route path={"/profile"} element={<Profile />} />
         </Route>
         <Route path={"/chat"} element={<Chat />} />
+        <Route path={"*"} element={<NotFound />} />
       </Routes>
     </ThemeProvider>
   );
