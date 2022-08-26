cc978ecc3c5b0bd03b1ae31a15c593bc5b5f0ff5
Tarxos <ftarxos@gmail.com>
2022-08-27 01:59:59 +0300

    inject redux persist

--- study/src/index.js
+++ study/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store/store";
+import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- study/src/store/store.js
+++ study/src/store/store.js
@@ -3,6 +3,9 @@ import authSlice from "./authSlice";
 import chatSlice from "./chatSlice";
 import counterReducer from "./counterSlice";
 import { createLogger } from "redux-logger";
+import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 const functionDelay = (store) => (next) => (action) => {
   const delay = action?.payload?.meta?.delay;
@@ -12,15 +15,33 @@ const functionDelay = (store) => (next) => (action) => {
 };
 
 const logger = createLogger({
-  duration: true
+  duration: true,
 });
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-    chat: chatSlice,
-    auth: authSlice,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger).concat(functionDelay),
+const reducers = combineReducers({
+  counter: counterReducer,
+  chat: chatSlice,
+  auth: authSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat(logger)
+      .concat(functionDelay),
   devTools: true,
 });
+export const persistor = persistStore(store);
