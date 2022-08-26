d2e6c941b2c2c80ff33ff1ff967ca9dc27fffdd9
Tarxos <ftarxos@gmail.com>
2022-08-27 00:17:05 +0300

    add function delay call middleware

--- study/src/store/store.js
+++ study/src/store/store.js
@@ -1,13 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import chatSlice from "./chatSlice";
 import counterReducer from "./counterSlice";
+import logger from "@reduxjs/logger";
+
+const functionDelay = (store) => (next) => (action) => {
+  const delay = action?.payload?.meta?.delay;
+  if (!delay) return next(action);
+  const dispose = setTimeout(() => next(action), 200);
+  return () => clearTimeout(dispose);
+};
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     chat: chatSlice,
-    auth: authSlice
+    auth: authSlice,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware()
+    .concat(functionDelay),
   devTools: true,
 });
