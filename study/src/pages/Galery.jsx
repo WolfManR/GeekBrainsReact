5a6c23435b068d92756dda6449bc4cf5f425768f
Tarxos <ftarxos@gmail.com>
2022-08-27 19:00:46 +0300

    add loading images on galery page

--- study/src/pages/Galery.jsx
+++ study/src/pages/Galery.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material'
 
 const Galery = () => {
+  const [galery, setGallery] = useState([])
+  const [error, setError] = useState('')
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/albums/1/photos')
+      .then((response) => {
+        if (response.ok) return response.json()
+        throw new Error('Cant load images')
+      })
+      .then((response) => setGallery(response))
+      .catch((e) => setError(e))
+  }, [])
+
+  if (error) {
+    return <div>{error.message}</div>
+  }
+
   return (
-    <div>Galery</div>
+    <ImageList variant="masonry" cols={5} gap={4}>
+      {galery.map((item) => (
+        <ImageListItem key={item.id}>
+          <img
+            src={`${item.thumbnailUrl}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.thumbnailUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar title={item.title} />
+        </ImageListItem>
+      ))}
+    </ImageList>
   )
 }
 
