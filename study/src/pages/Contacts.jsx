715f19a2ef4170dfddc29df718e4f8c1f3d5fc97
Tarxos <ftarxos@gmail.com>
2022-09-03 20:32:54 +0300

    add missed add behavior for contacts list

--- study/src/pages/Contacts.jsx
+++ study/src/pages/Contacts.jsx
@@ -1,38 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomLink from '../components/customLink/CustomLink'
 const contacts = [
   { id: 1, name: 'John', year: 2000 },
   { id: 2, name: 'Lena', year: 2001 },
 ]
 const Contacts = () => {
+  const [name, setName] = useState('')
   const deleteContact = (id) => {
     console.log(id)
   }
+  const addContact = () => {}
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>id</td>
-          <td>name</td>
-          <td>year</td>
-          <td></td>
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact) => (
-          <tr key={contact.id}>
-            <td>{contact.id}</td>
-            <td>{contact.name}</td>
-            <td>{contact.year}</td>
-            <td>
-              <CustomLink to={`/contact/${contact.id}`}>details</CustomLink>
-              <button onClick={() => deleteContact(contact.id)}>delete</button>
-            </td>
+    <div>
+      <div>
+        <label>name</label>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={addContact}>Add</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>year</td>
+            <td></td>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>{contact.id}</td>
+              <td>{contact.name}</td>
+              <td>{contact.year}</td>
+              <td>
+                <CustomLink to={`/contact/${contact.id}`}>details</CustomLink>
+                <button onClick={() => deleteContact(contact.id)}>
+                  delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
