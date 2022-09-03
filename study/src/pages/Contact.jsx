345ac220ae96454e2fb15275f881ffaa776bd7bf
Tarxos <ftarxos@gmail.com>
2022-09-03 20:24:01 +0300

    prototype contacts list behaviors

--- study/src/pages/Contact.jsx
+++ study/src/pages/Contact.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import CustomLink from '../components/customLink/CustomLink'
+
+const contact = { id: 1, name: 'John', email: 'john@bk.ru' }
 
 const Contact = () => {
-    const { id } = useParams()
-  return <div>{id}</div>
+  const { id } = useParams()
+  const [isEditForm, setIsEditForm] = useState(false)
+
+  const asEdit = () => {
+    setIsEditForm(true)
+  }
+
+  const Save = () => {
+    setIsEditForm(false)
+  }
+  const deleteContact = (id) => {}
+
+  const renderDetailsForm = () => {
+    return (
+      <div>
+        <div>
+          <label>name </label>
+          <label>{contact.name}</label>
+          <br />
+          <label>email </label>
+          <label>{contact.email}</label>
+        </div>
+        <div>
+          <button onClick={asEdit}>Edit</button>
+          <button onClick={() => deleteContact(id)}>Delete</button>
+        </div>
+      </div>
+    )
+  }
+  const renderEditForm = () => {
+    return (
+      <div>
+        <label>name </label>
+        <input value={contact.name} />
+        <br />
+        <label>email </label>
+        <input value={contact.email} />
+        <br />
+        <button onClick={Save}>Save</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div>
+        <CustomLink to="/contacts">To List</CustomLink>
+      </div>
+      id:{id}
+      {isEditForm ? renderEditForm() : renderDetailsForm()}
+    </div>
+  )
 }
 
 export default Contact
--- study/src/pages/Contacts.jsx
+++ study/src/pages/Contacts.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import CustomLink from '../components/customLink/CustomLink'
+const contacts = [
+  { id: 1, name: 'John', year: 2000 },
+  { id: 2, name: 'Lena', year: 2001 },
+]
 const Contacts = () => {
+  const deleteContact = (id) => {
+    console.log(id)
+  }
+
   return (
-    <div>Contacts</div>
+    <table>
+      <thead>
+        <tr>
+          <td>id</td>
+          <td>name</td>
+          <td>year</td>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map((contact) => (
+          <tr key={contact.id}>
+            <td>{contact.id}</td>
+            <td>{contact.name}</td>
+            <td>{contact.year}</td>
+            <td>
+              <CustomLink to={`/contact/${contact.id}`}>details</CustomLink>
+              <button onClick={() => deleteContact(contact.id)}>delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
